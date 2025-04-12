45a6cc432b3074ff0cd17bbb6c88a8ba819bd136
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-04-12 21:12:01 +0100

    improvements to testimonials and about stats

--- components/about/AboutStats.tsx
+++ components/about/AboutStats.tsx
@@ -8,38 +8,44 @@ const AboutStats = () => {
   const inView = useInView(ref, { once: true });
   return (
     <div ref={ref} className="flex justify-center">
-      <ul className="flex flex-wrap list-none">
-        <li className="block">
-          <span className="font-lora  text-5xl text-primary">
-            {inView ? (
-              <motion.span
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-              >
-                <CountUp start={0} end={10} />
-                <span>+</span>
-              </motion.span>
-            ):0}
-          </span>
-          <p>Ongoing Projects</p>
+      <ul className="flex flex-wrap list-none gap-8">
+        <li className="flex-grow flex justify-center">
+          <div className="space-y-4">
+            <span className="font-lora  text-5xl text-primary">
+              {inView ? (
+                <motion.span
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <CountUp start={0} end={10} />
+                  <span>+</span>
+                </motion.span>
+              ) : (
+                0
+              )}
+            </span>
+            <p>Ongoing Projects</p>
+          </div>
         </li>
-        <li className="block pl-[30px] sm:pl-[40px] md:pl-[60px]">
-          <span className="font-lora  text-5xl text-primary">
-            {inView ? (
-              <motion.span
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-              >
-                <CountUp start={0} end={50} />
-                <span>+</span>
-              </motion.span>
-            ) : (
-              0
-            )}
-          </span>
-          <p>Completed Projects</p>
+        <li className="flex-grow flex justify-center">
+          <div className="space-y-4">
+            <span className="font-lora  text-5xl text-primary">
+              {inView ? (
+                <motion.span
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <CountUp start={0} end={50} />
+                  <span>+</span>
+                </motion.span>
+              ) : (
+                0
+              )}
+            </span>
+            <p>Completed Projects</p>
+          </div>
         </li>
       </ul>
     </div>
--- components/home/TestimonialsSection.tsx
+++ components/home/TestimonialsSection.tsx
@@ -1,118 +1,90 @@
 "use client";
-import { motion } from "framer-motion";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+import { Navigation } from "swiper/modules";
+import { FiUser } from "react-icons/fi";
 
 const TestimonialsSection = () => {
-  const [carouselIndex, setCarouselIndex] = useState(0);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [parentWidth, setParentWidth] = useState(0);
-  const [xPosition, setXPosition] = useState(0);
-
-  useEffect(() => {
-    if (containerRef.current) {
-      setParentWidth(containerRef.current.offsetWidth + 20);
-    }
-    const handleResize = () => {
-      if (containerRef.current) {
-        setParentWidth(containerRef.current.offsetWidth + 20);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    setXPosition(carouselIndex * Number(parentWidth));
-  }, [carouselIndex, parentWidth]);
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
 
   const content = [
     {
-      name: "Mr. Temitope Shunobi  Co-founder and Executive Director of Sahara Group",
+      name: "Mr. Temitope Shunobi",
       comment: (
         <>
-         I am impressed with Albert and his work in my house. He is young and knows what he is looking for in life, I recommend him.
+          I am impressed with Albert and his work in my house. He is young and
+          knows what he is looking for in life, I recommend him.
         </>
       ),
+      company: "Co-founder and Executive Director of Sahara Group",
     },
     {
-      name: "Mr. Samson Ghani Kato: Sahara Group",
+      name: "Mr. Samson Ghani Kato",
       comment: (
         <>
-          Albert and his company are doing very well. I am very impressed by their work ethic and integrity. I highly recommend AV.
-
+          Albert and his company are doing very well. I am very impressed by
+          their work ethic and integrity. I highly recommend AV.
         </>
       ),
+      company: "Sahara Group",
     },
     {
-      name: "Mr. Seun: M.D Centurium",
-      comment: (
-        <>
-           I recommend AV.
-
-        </>
-      ),
+      name: "Mr. Seun",
+      comment: <>I recommend AV.</>,
+      company: "M.D Centurium",
     },
     {
       name: "Pst. Engr. Opeyemi J",
       comment: (
         <>
-         AV Construction is simply the best choice for construction. Full of young and talented individuals.
+          AV Construction is simply the best choice for construction. Full of
+          young and talented individuals.
         </>
       ),
     },
     {
-      name: "Emmanuel C. Vincent",
+      name: "Mr.  Kenna",
+      comment: <>Your work is sharp, topnotch and I love it.</>,
+      company: "GreenVille LNG",
+    },
+    {
+      name: "Mr. Nabil",
       comment: (
         <>
-         OMO baba wuna too sabi. Highly recommended always.
-
+          Thank you Mr. Albert and the entire team for a work well-done. I am
+          very grateful.
         </>
       ),
+      company: "Greenville LNG",
+    },
+    {
+      name: "Mr. Idris",
+      comment: <>Superb and excellent work.</>,
+      company: "M.D Aik concepts LTD",
+    },
+    {
+      name: "Emmanuel C. Vincent",
+      comment: <>OMO baba wuna too sabi. Highly recommended always.</>,
     },
     {
       name: "Birma J. J",
       comment: (
         <>
-       Wow, you guys have really gone so far in such a short time. I am very impressed and would surely recommend you my friend.
-
-
-        </>
-      ),
-    },
-     {
-      name: "Mr.  Kenna: GreenVille LNG",
-      comment: (
-        <>
-       Your work is sharp, topnotch and I love it.
-        </>
-      ),
-    },
-     {
-      name: "Mr. Nabil: Greenville LNG",
-      comment: (
-        <>
-     Thank you Mr. Albert and the entire team for a work well-done. I am very grateful.
-        </>
-      ),
-    },
-     {
-      name: "Mr. Idris: M.D Aik concepts LTD",
-      comment: (
-        <>
-     Superb and excellent work.
+          Wow, you guys have really gone so far in such a short time. I am very
+          impressed and would surely recommend you my friend.
         </>
       ),
     },
     {
-      name: "Mrs. Susuan Kunle",
+      name: "Mrs. Susuan Kunle",
       comment: (
         <>
-    I am glad Mr. Samson recommended you and not someone else. I love the final output and would surely call you guys back.
-
+          I am glad Mr. Samson recommended you and not someone else. I love the
+          final output and would surely call you guys back.
         </>
       ),
     },
@@ -121,93 +93,81 @@ const TestimonialsSection = () => {
   return (
     <div className="">
       <div className="col-span-12 relative">
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        // modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-      </Swiper>
-        <div className="pb-[40px] md:pb-0 overflow-hidden ">
-          
-          <motion.div
-            className="flex gap-3 min-w-max p-10"
-            animate={{
-              x: -xPosition,
-            }}
-            transition={{ type: "tween" }}
-          >
-            {content.map((each, index) => (
-              <motion.div
-                drag="x"
-                dragConstraints={{ left: 0, right: 0 }}
-                onDragEnd={(event, info) => {
-                  if (info.offset.x > 100 && carouselIndex !== 0) {
-                    setCarouselIndex(carouselIndex - 1); // Swiped left
-                  } else if (
-                    info.offset.x < -100 &&
-                    carouselIndex !== content.length - 1
-                  ) {
-                    setCarouselIndex(carouselIndex + 1); // Swiped right
-                  }
-                }}
-                className="relative w-[80vw] sm:max-w-sm"
-                ref={containerRef}
-                // style={{ width: parentWidth }}
-                key={index}
-              >
-                {/* shape and images */}
-                <div className=" p-5 shadow-lg space-y-3">
-                  <p className="font-bold text-2xl">Title</p>
-                  <p className="text-foreground">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Quisquam dolorem assumenda perspiciatis neque, ad quibusdam
-                    est excepturi
-                  </p>
+        <Swiper
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            720: {
+              slidesPerView: 2,
+            },
+            1204: {
+              slidesPerView: 3,
+            },
+          }}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation]}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onBeforeInit={(swiper) => {
+            // Swiper expects the navigation elements to exist before init
+            // So we set them manually like this
+
+            setTimeout(() => {
+              if (
+                prevRef.current &&
+                nextRef.current &&
+                swiper.params.navigation
+              ) {
+                const navigationOptions = swiper.params.navigation as any;
+                navigationOptions.prevEl = prevRef.current;
+                navigationOptions.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }
+            });
+          }}
+        >
+          {content.map((each, index) => (
+            <SwiperSlide key={index}>
+              <div className="h-80 md:h-56">
+                <div className=" p-5 shadow-lg space-y-3 h-56 md:h-48 flex gap-3 flex-col">
+                  <p className="text-foreground flex-grow">{each.comment}</p>
                   <div className="flex gap-2 items-center">
-                    <Image
-                      className="rounded-full size-12"
-                      src="https://dummyimage.com/100x100"
-                      alt="testimonial"
-                      width={100}
-                      height={100}
-                    />
+                    <span className="rounded-full text-white bg-secondary  grid items-center p-2">
+                      <FiUser size={20} />
+                    </span>
                     <div className="space-y-1 text-sm">
                       <p className="font-bold">{each.name}</p>
-                      <p className="text-primary">Company Name</p>
+                      <p className="text-primary">{each.company}</p>
                     </div>
                   </div>
                 </div>
-              </motion.div>
-            ))}
-          </motion.div>
-          {/* If we need navigation buttons */}
-          <div className="absolute bottom-0 md:-bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-x-3  md:block">
-            <button
-              type="button"
-              onClick={() =>
-                carouselIndex !== 0 ? setCarouselIndex(carouselIndex - 1) : null
-              }
-              className=" p-3 rounded-full bg-primary  text-white hover:bg-secondary top-auto bottom-[85px] left-[30px] "
-            >
-              <FaChevronLeft />
-            </button>
-            <button
-              type="button"
-              onClick={() =>
-                carouselIndex !== content.length - 1
-                  ? setCarouselIndex(carouselIndex + 1)
-                  : null
-              }
-              className="p-3 rounded-full bg-primary  text-white hover:bg-secondary top-auto bottom-[85px] left-[85px]"
-            >
-              <FaChevronRight />
-            </button>
-          </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* If we need navigation buttons */}
+        <div className="absolute bottom-0 md:-bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-x-3  md:block z-40">
+          <button
+            type="button"
+            ref={prevRef}
+            className=" p-3 rounded-full bg-primary  text-white hover:bg-secondary top-auto bottom-[85px] left-[30px] disabled:opacity-40 disabled:cursor-not-allowed"
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            ref={nextRef}
+            type="button"
+            className="p-3 rounded-full bg-primary  text-white hover:bg-secondary top-auto bottom-[85px] left-[85px] disabled:opacity-40 disabled:cursor-not-allowed"
+          >
+            <FaChevronRight />
+          </button>
         </div>
       </div>
     </div>
