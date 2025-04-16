fcef235491cf4a64dad46bc77218ee41c07d19ac
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-04-17 00:33:17 +0100

    embeded map

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -18,7 +18,7 @@ export default function ContactPage() {
         title="Contact Us"
         links={[{ label: "Contact", href: "/contact" }]}
       />
-      <Container className="text-foreground space-y-10">
+      <Container className="text-foreground space-y-14">
         <div className="space-y-4">
           <h2 className="h3 text-primary text-4xl font-bold">
             Send us a message
@@ -28,8 +28,22 @@ export default function ContactPage() {
             from you.
           </p>
         </div>
-        <div className=" max-w-3xl space-y-4">
-          <ContactForm />
+        <div className="flex items-center gap-6">
+          <div className="space-y-4 flex-grow">
+            <ContactForm />
+          </div>
+          <div className="w-2/5">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d985.0663502957254!2d7.504900587676614!3d9.039537446740717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0beced85da1f%3A0x8c4f7fa102315c49!2s13%20Onitsha%20Cres%2C%20Area%2C%2011%20900242%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1744845633074!5m2!1sen!2sng"
+              // width={600}
+              // height={450}
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className="h-[450px] w-full"
+            />
+          </div>
         </div>
         <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] text-foreground gap-5  mx-auto">
           <Link
