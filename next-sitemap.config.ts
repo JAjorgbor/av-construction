05b65e53dc921e433f05530bcfff25f0ccb2019b
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2025-04-17 15:51:38 +0100

    updated sitemap config

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.example.com",
+  siteUrl: "https://www.avconstructionsltd.com",
   generateRobotsTxt: true,
 };
