b4526e08c054afad486b1c23fe7a1afc60d7e784
nathanieluriri <95766173+nathanieluriri@users.noreply.github.com>
2025-04-21 16:02:43 +0100

    Update page.tsx
    ADDED NEW MEMBERS OF THE TEAM

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -233,6 +233,96 @@ export default function AboutPage() {
                 </p>
               </div>
             </div>
+             <div className="space-y-3 bg-white drop-shadow-xl">
+              <Image
+                src="/media/ceo.jpg"
+                alt="ARC. ATHANASIUS MANJI ALBERT"
+                className="w-full object-cover h-[350px]"
+                width={800}
+                height={800}
+              />
+              <div className="space-y-2 flex flex-col flex-grow divide-y">
+                <div className="p-3 flex-grow">
+                  <p className="font-semibold text-lg">
+                    ARC. ATHANASIUS MANJI ALBERT
+                  </p>
+                  <p className="text-primary">Founder and M.D</p>
+                </div>
+                <p className="p-3 flex justify-center h-12">
+                  {/* <span className="font-semibold">Contact: </span> */}
+                  <Link
+                    target="_blank"
+                    href={"mailto:md@avconstructions.com"}
+                    aria-label="message md@avconstructions.com"
+                    className="text-primary hover:underline"
+                  >
+                    <FiMail size={25} />
+                  </Link>
+                </p>
+              </div>
+            </div>
+
+
+
+             <div className="space-y-3 bg-white drop-shadow-xl">
+              <Image
+                src="/media/emoseh.jpg"
+                alt="ARC. EMOSEN T. MBWARA"
+                className="w-full object-cover h-[350px]"
+                width={800}
+                height={800}
+              />
+              <div className="space-y-2 flex flex-col flex-grow divide-y">
+                <div className="p-3 flex-grow">
+                  <p className="font-semibold text-lg">
+                    ARC. EMOSEN T. MBWARA
+                  </p>
+                  <p className="text-primary">Senior Architect</p>
+                </div>
+                <p className="p-3 flex justify-center h-12">
+                  {/* <span className="font-semibold">Contact: </span> */}
+                  <Link
+                    target="_blank"
+                    href={"mailto:info@avconstructions.com"}
+                    aria-label="message info@avconstructions.com"
+                    className="text-primary hover:underline"
+                  >
+                    <FiMail size={25} />
+                  </Link>
+                </p>
+              </div>
+            </div>
+
+
+
+             <div className="space-y-3 bg-white drop-shadow-xl">
+              <Image
+                src="/media/eng-yusuf-m-yusuf.png"
+                alt="ENG. YUSUF M. YUSUF"
+                className="w-full object-cover h-[350px]"
+                width={800}
+                height={800}
+              />
+              <div className="space-y-2 flex flex-col flex-grow divide-y">
+                <div className="p-3 flex-grow">
+                  <p className="font-semibold text-lg">
+                    ENG. YUSUF M. YUSUF
+                  </p>
+                  <p className="text-primary">Senior Engineer</p>
+                </div>
+                <p className="p-3 flex justify-center h-12">
+                  {/* <span className="font-semibold">Contact: </span> */}
+                  <Link
+                    target="_blank"
+                    href={"mailto:info@avconstructionsltd.com"}
+                    aria-label="message info@avconstructionsltd.com.com"
+                    className="text-primary hover:underline"
+                  >
+                    <FiMail size={25} />
+                  </Link>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
